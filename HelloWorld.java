c30f4e35e075465322c7e602c636c269f3760a51
Velan987 <83496854+Velan987@users.noreply.github.com>
2021-08-12 11:33:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args) {
+		System.out.println("Hello from Git Repo");
 		String str = "USD50,000.21";
 		String numberStr = str.replaceAll("[^0-9.]","");
 		System.out.println(numberStr);
